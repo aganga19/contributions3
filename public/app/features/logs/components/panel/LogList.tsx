4b4b3385b6b36352e04a71c9ed2fa45b16a6f865
Matias Chomicki <matyax@gmail.com>
2025-09-15 18:33:35 +0200

    Remove hardcoded true

--- public/app/features/logs/components/panel/LogList.tsx
+++ public/app/features/logs/components/panel/LogList.tsx
@@ -147,7 +147,7 @@ export const LogList = ({
   setDisplayedFields,
   showControls,
   showTime,
-  showUniqueLabels = true,
+  showUniqueLabels,
   sortOrder,
   syntaxHighlighting = logOptionsStorageKey ? store.getBool(`${logOptionsStorageKey}.syntaxHighlighting`, true) : true,
   timeRange,
